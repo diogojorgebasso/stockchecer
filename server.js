7b68dcae250958cc8a72fbb153153793e319a07e
Diogo Basso <diogojbasso@gmail.com>
2021-03-20 00:10:32 +0000

    fix: rate-limiter && homepage

--- server.js
+++ server.js
@@ -19,13 +19,18 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
 
+// apply rate limiter to all requests
+app.use(limiter);
 //Index page (static HTML)
-app.get('/:path', function (req, res) {
-  const path = req.params.path
-    if (isValidPath(path)){
+app.get('/', function (req, res) {
       res.sendFile(process.cwd() + '/views/index.html');
-    }
   });
 
 //For FCC testing purposes
